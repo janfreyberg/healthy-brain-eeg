213b862128cfdcc3a97a0e160cb8becd0696b27f
Jan Freyberg <jan.freyberg@gmail.com>
2017-06-20 17:29:03 +0100

    Update some of the data import scripts

--- data/raw/resting.py
+++ data/raw/resting.py
@@ -10,53 +10,50 @@ from data import datafolder
 # the types of files this dataset has
 filetypes = ['chanlocs', 'event', 'data']
 
-# make a list of the resting state files
-restfiles = {}
-for key in filetypes:
-    restfiles[key] = list(datafolder.glob(
-        pattern=f'*/EEG/preprocessed/csv_format/RestingState_{key}.csv'
-    ))
-
-# add the IDs to this dictionary
-restfiles['id'] = [s.parts[-5] for s in restfiles['chanlocs']]
+datafiles = list(datafolder.glob(
+    pattern='*/EEG/raw/csv_format/*RestingState_data.csv'
+))
+eventfiles = list(datafolder.glob(
+    pattern='*/EEG/raw/csv_format/*RestingState_event.csv'
+))
 
 
-# number of rest files
-n = len(restfiles['id'])
+# number of files
+n = len(datafiles)
 
 
 # implement the raw data structures as a generator
 # so that the code isn't run >400x just on import
 def raws():
-    for idx in range(len(restfiles['id'])):
+    for datafile in datafiles:
         # read the channel locations
-        chanlocs = pd.read_csv(restfiles['chanlocs'][idx])
-        ch_names = list(chanlocs['labels'])
-        pos = np.array(chanlocs.loc[:, ('X', 'Y', 'Z')])
+        # chanlocs = pd.read_csv(restfiles['chanlocs'][idx])
+        # ch_names = list(chanlocs['labels'])
+        # pos = np.array(chanlocs.loc[:, ('X', 'Y', 'Z')])
         # make a montage from the chanlocs
-        mtg = mne.channels.Montage(
-            pos, ch_names, 'custom', range(len(ch_names))
-        )
+        # mtg = mne.channels.Montage(
+        #     pos, ch_names, 'custom', range(len(ch_names))
+        # )
         # make an info structure
-        info = mne.create_info(ch_names=ch_names, sfreq=500,
-                               ch_types='eeg', montage=mtg)
+        info = mne.create_info(ch_names=111, sfreq=500,
+                               ch_types='eeg', montage=None)
         # load the data from the text file
-        data = np.genfromtxt(restfiles['data'][idx], delimiter=',')
+        data = np.genfromtxt(datafile, delimiter=',')
         # find bad electrodes
-        badbool = np.all(data == 0, axis=1)
-        badlist = [ch for b, ch in zip(badbool.ravel(), ch_names) if b]
+        # badbool = np.all(data == 0, axis=1)
+        # badlist = [ch for b, ch in zip(badbool.ravel(), ch_names) if b]
         # make the raw data structure
         raw = mne.io.RawArray(data, info)
 
         # add some cool info
-        raw.info['subject_info'] = restfiles['id'][idx]
-        raw.info['bads'] = badlist
+        raw.info['subject_info'] = datafile.name[0:12]
+        # raw.info['bads'] = badlist
 
         yield raw
 
 
 # make a generator for the events, read from file
-def events():
+def restevents():
     for idx in range(len(restfiles['id'])):
         # load the events from file
         eventdf = pd.read_csv(restfiles['event'][idx])
--- data/raw/surround_suppression.py
+++ data/raw/surround_suppression.py
@@ -10,63 +10,37 @@ from data import datafolder
 # the types of files this dataset has
 filetypes = ['chanlocs', 'event', 'data']
 
-# make a list of the csv files
-
-datafiles = list(datafolder.glob(
-    pattern='*/EEG/preprocessed/csv_format/*SurroundSupp*data.csv'
-))
-eventfiles = list(datafolder.glob(
-    pattern='*/EEG/preprocessed/csv_format/*SurroundSupp*event.csv'
-))
-chanlocfiles = list(datafolder.glob(
-    pattern='*/EEG/preprocessed/csv_format/*SurroundSupp*chanlocs.csv'
-))
-stimulusfiles = list(datafolder.glob(
-    pattern='*/Behavioral/csv_format/*SurroundSupp*.csv'
-))
-
-pids = list(set(
-    [f.parts[-5] for f in datafiles]
-))
-
-datafiles = {
-    pid: [f for f in datafiles if pid in f.parts]
-    for pid in pids
-}
-eventfiles = {
-    pid: [f for f in eventfiles if pid in f.parts]
-    for pid in pids
-}
-chanlocfiles = {
-    pid: [f for f in chanlocfiles if pid in f.parts]
-    for pid in pids
-}
-stimulusfiles = {
-    pid: [f for f in stimulusfiles if pid in f.parts]
-    for pid in pids
-}
+# make a list of the resting state files
+csvfiles = {}
+for key in filetypes:
+    csvfiles[key] = [tup for tup in zip(
+        list(datafolder.glob(
+            pattern=f'*/EEG/preprocessed/csv_format/SurroundSupp_Block1_{key}.csv')),
+        list(datafolder.glob(
+            pattern=f'*/EEG/preprocessed/csv_format/SurroundSupp_Block2_{key}.csv'))
+    )]
+csvfiles['triggerkeys'] = [tup for tup in zip(
+    list(datafolder.glob(
+        pattern=f'*/Behavioral/csv_format/*SurroundSupp_Block1.csv')),
+    list(datafolder.glob(
+        pattern=f'*/Behavioral/csv_format/*SurroundSupp_Block2.csv'))
+)]
 
 # add the IDs to this dictionary
-n = len(datafiles)
+csvfiles['id'] = [s[0].parts[-5] for s in csvfiles['chanlocs']]
+n = len(csvfiles['id'])
 
 
 # implement the epoch data structures as a generator
 # so that the code isn't run >400x just on import
 def epochs():
-    for pid in pids:
+    for subject in range(len(csvfiles['id'])):
 
-        # how many files there are for this subject:
-        n_subject = len(datafiles[pid])
-
-        # empty lists for appending
         epochs = []
-        badlist = []
-
-        for block in range(n_subject):
+        badlists = []
+        for block in range(2):
             # read the channel locations:
-            chanlocs = pd.read_csv(
-                chanlocfiles[pid][block]
-            )
+            chanlocs = pd.read_csv(csvfiles['chanlocs'][subject][block])
             ch_names = list(chanlocs['labels'])
             pos = np.array(chanlocs.loc[:, ('X', 'Y', 'Z')])
             # make a montage from the chanlocs
@@ -77,47 +51,46 @@ def epochs():
             # make info structure
             info = mne.create_info(ch_names=ch_names, sfreq=500,
                                    ch_types='eeg', montage=mtg)
-            info['subject_info'] = pid
-
             # load the data from the text file
             data = np.genfromtxt(
-                datafiles[pid][block], delimiter=',')
+                csvfiles['data'][subject][block], delimiter=',')
             # make a raw structure
             raw = mne.io.RawArray(data, info)
             # find bad electrodes
             badbool = np.all(data == 0, axis=1)
-
-            badlist = list(set(
-                badlist + [ch for b, ch in zip(badbool.ravel(), ch_names)
-                           if b]
-            ))
+            badlists.append(
+                [ch for b, ch in zip(badbool.ravel(), ch_names) if b]
+            )
+            # add some cool info
+            raw.info['subject_info'] = csvfiles['id'][subject]
 
             # read the events file and the behav file
-            eventdf = pd.read_csv(eventfiles[pid][block])
-            decodedf = pd.read_csv(stimulusfiles[pid][block])
-
+            eventdf = pd.read_csv(csvfiles['event'][subject][block])
+            decodedf = pd.read_csv(csvfiles['triggerkeys'][subject][block])
             # drop superfluous events
             eventdf = eventdf.loc[eventdf['type'] == '8   ', :]
             # construct a new trigger code:
-            eventcode = np.array([cntcon * 100 + bgcon
-                                  for cntcon, bgcon in
-                                  zip(decodedf['CNTcon'], decodedf['BGcon'])
-                                  ])
+            eventdf['type'] = np.array(decodedf['BGcon'] * 100 +
+                                       decodedf['CNTcon'] * 10 +
+                                       decodedf['StimCond'] * 1)
             eventarray = np.array([eventdf.loc[:, 'sample'],
                                    np.zeros(eventdf.shape[0]),
-                                   eventcode],
+                                   eventdf.loc[:, 'type']],
                                   dtype='int').T
 
             # make the epoch structure
             epochs.append(mne.Epochs(raw, eventarray, tmin=0, tmax=3))
 
+        badlist = list(set(badlists[0] + badlists[1]))
+
+        epochs[0].info['bads'] = badlist
+        epochs[1].info['bads'] = badlist
         # concatenate the two epoch structs since they are equivalent
         epoch = mne.concatenate_epochs(epochs)
-        epoch.info['bads'] = badlist
 
         yield epoch
 
-
+        
 def raws(block=1):
     for subject in range(len(csvfiles['id'])):
         # read the channel locations:
@@ -142,5 +115,5 @@ def raws(block=1):
         raw.info['bads'] = [ch for b, ch in zip(badbool.ravel(), ch_names) if b]
         # add some cool info
         raw.info['subject_info'] = csvfiles['id'][subject]
-
+        
         yield raw
