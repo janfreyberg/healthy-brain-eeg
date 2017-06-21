f430ed1cd238d7b14eed54a53069c08811869c2f
Jan Freyberg <jan.freyberg@gmail.com>
2017-06-21 10:41:25 +0100

    Change download script to use requests for download
    This enables a progress bar

--- data/download.py
+++ data/download.py
@@ -4,6 +4,7 @@ from botocore.client import Config
 
 from data import datafolder
 
+import requests
 import os
 import random  # random sampling
 from pathlib import Path  # path operations
@@ -12,23 +13,42 @@ from tqdm import tqdm  # progress bar
 
 s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
 
-all_remote_files = s3.list_objects(Bucket='fcp-indi',
-                                   Prefix='data/Projects/HBN/S1/EEG/')
+all_remote_files = s3.list_objects(
+    Bucket='fcp-indi', Prefix='data/Projects/HBN/S1/EEG/'
+)
 
 n_remote = len(all_remote_files['Contents'])
 
 def download_all():
     print(f"Downloading {n_remote} random data sets.")
-    for fileobj in tqdm(all_remote_files['Contents']):
-        s3.download_file('fcp-indi', fileobj['Key'],
-                         str(datafolder / Path(fileobj['Key']).name))
+    for fileobj in tqdm(all_remote_files['Contents'], desc='Files'):
+        # request the file
+        requests.get(f"https://s3.amazonaws.com/fcp-indi/{fileobj['Key']}",
+                     stream=True)
+        
+        total_length = int(r.headers.get('content-length'))
+        
+        with open(datafolder / Path(fileobj['Key']).name, 'wb+') as f:
+            for chunk in tqdm(r.iter_content(chunk_size=1024),
+                              total=(total_length / 1024), desc='Bytes'): 
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
 
 
 def download_sample(n=10):
     print(f"Downloading {n} random data sets.")
-    for fileobj in tqdm(random.sample(all_remote_files['Contents'], n)):
-        s3.download_file('fcp-indi', fileobj['Key'],
-                         str(datafolder / Path(fileobj['Key']).name))
+    for fileobj in tqdm(random.sample(all_remote_files['Contents'], n), desc='Files'):
+        # request the file
+        r = requests.get(f"http://s3.amazonaws.com/fcp-indi/{fileobj['Key']}",
+                         stream=True)
+        
+        total_length = int(r.headers.get('content-length'))
+        
+        with open(datafolder / Path(fileobj['Key']).name, 'wb+') as f:
+            for chunk in tqdm(r.iter_content(chunk_size=1024),
+                              total=(total_length / 1024), desc='Bytes'):
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
 
 
 def extract_all(delete_archives=True):
